d5ef1c5974c054fdf8fc30628d0c08ea54916865
MR-NOKIB <nozomuddinnokib03@gmail.com>
2024-09-19 23:13:27 +0100

    error-page, few more hooks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import "./Header.css"
 
 const Header = () => {
     return (
-        <div className='header'>
-            <Link to="/">Home</Link>
-            <Link to="/users">Users</Link>
-            <Link to="/posts">Posts</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">contact</Link>
-        </div>
+        <nav className='header'>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/users">Users</NavLink>
+            <NavLink to="/posts">Posts</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/contact">contact</NavLink>
+        </nav>
     );
 };
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,14 +2,20 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useNavigation } from 'react-router-dom';
 
 const Home = () => {
+    const navigation = useNavigation();
+    const location = useLocation();
+    console.log(location)
     return (
         <div>
             <h2>Helloooo from root/home</h2>
             <Header></Header>
-            <Outlet></Outlet>
+            {
+                navigation.state === 'loading' ? 'Loading...' :  <Outlet></Outlet>
+            }
+           
             <Footer></Footer>
         </div>
     );
